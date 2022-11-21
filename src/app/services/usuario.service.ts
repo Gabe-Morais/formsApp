2e13b37214935baac255d6d8dfb628e599eb66cb
Gabriel Morais <gabe-morais@outlook.com>
2022-11-21 20:39:03 -0300

    Finalizando projeto Ionic

--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -13,35 +13,49 @@ export class UsuarioService {
   ];
 
   // Sempre declarar o servico no construtor "variavel: NomeDaClasse"
-  constructor(private storageService : StorageService) {}
+  constructor(private storageService: StorageService) { }
+
+  async login(email: string, senha: string) {
+    this.buscarTodos();
+    let usuario: Usuario;
+    this.listaUsuarios.filter(item => {
+      if (item.email.toLocaleLowerCase() == email.toLocaleLowerCase()) { }
+      usuario = item;
+    });
+    if (usuario?.senha === senha) {
+      return usuario;
+    }
+    return null;
+  }
+
 
   //salvar usurio dentro da lista de usuarios.
-  async salvar(usuario:Usuario){
-    this.listaUsuarios[usuario.id]= usuario;
+  async salvar(usuario: Usuario) {
+    this.listaUsuarios[usuario.id] = usuario;
     await this.storageService.set('usuarios', this.listaUsuarios);
   }
 
-  async buscarUm(){}
+  async buscarUm() { }
 
   // "listaUsuarios recebe a busca por "usuarios" / "as unknown as Usuario[]" caso nao tenho nd retorne vazio ou o usuario "
-  async buscarTodos(){
+  async buscarTodos() {
     this.listaUsuarios = await this.storageService.get('usuarios') as unknown as Usuario[];
-    if(!this.listaUsuarios){
+    if (!this.listaUsuarios) {
       return [];
     }
     return this.listaUsuarios;
   }
 
-  async deletar(){}
+  async deletar() { }
 
   // Armazenamos o idUsuario 
-  async salvarId(id: number){
+  async salvarId(id: number) {
     await this.storageService.set('idUsuario', id);
   }
   //armazenamos nossa busca dentro de uma constante, e aplicamos uma condição para caso o id exista nos retorne ele.
-  async buscarId(){
-    const id =  await this.storageService.get('idUsuario');
-    if(!id){
+  async buscarId() {
+    const id = await this.storageService.get('idUsuario');
+    if (!id) {
       return 0;
     }
     return id;
